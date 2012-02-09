122357c55e45406f168275a06b8d789c0bf6424d
Erik Kallen <info@erikkallen.nl>
2012-02-09 13:52:46 +0100

    renamed stuff

--- lib/processingjs-source.rb
+++
@@ -1,9 +0,0 @@
-require "processingjs-source/version"
-
-module Processingjs
-  module Source
-    def self.bundled_path
-      File.expand_path("processingjs-source/processingjs.js", __FILE__)
-    end
-  end
-end
--- lib/processingjs-source/version.rb
+++
@@ -1,5 +0,0 @@
-module Processingjs
-  module Source
-    VERSION = "0.0.1"
-  end
-end
